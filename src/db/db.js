4174166f77bfafa7df0ab9001fe3b4539c08631e
Muhammad Moiz <moizm5410@gmail.com>
2026-08-04 02:41:33 +0500

    revert debug changes - back to clean version

--- src/db/db.js
+++ src/db/db.js
@@ -3,12 +3,6 @@ import { DB_Name } from "../constants.js";
 
 const connectDB = async () => {
   try {
-
-    console.log("ALL ENV VARS:", process.env) // ← log everything
-    console.log("TEST_VAR:", process.env.TEST_VAR)
-    console.log("MONGODB_URI:", process.env.MONGODB_URI)
-
-    
     const connectionInstance = await mongoose.connect(
       `${process.env.MONGODB_URI}/${DB_Name}`
     );
